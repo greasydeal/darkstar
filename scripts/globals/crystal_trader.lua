631caa9e95a76905b1c598a2ac5437ebb2724c71
goatfinger <goatfinger@hotmail.com>
2014-06-17 14:01:04 -0500

    crys

--- scripts/globals/crystal_trader.lua
+++ scripts/globals/crystal_trader.lua
@@ -11,9 +11,10 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4104);
 			player:messageSpecial(ITEM_OBTAINED,4104);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4097,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -21,9 +22,10 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4105);
 			player:messageSpecial(ITEM_OBTAINED,4105);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4098,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -31,9 +33,10 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4106);
 			player:messageSpecial(ITEM_OBTAINED,4106);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4099,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -41,19 +44,20 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4107);
 			player:messageSpecial(ITEM_OBTAINED,4107);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4100,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
 		if(player:getFreeSlotsCount() >= 1) then 
 			player:tradeComplete();
 			player:addItem(4108);
-			player:messageSpecial(ITEM_OBTAINED,4108);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4101,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -61,9 +65,10 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4109);
 			player:messageSpecial(ITEM_OBTAINED,4109);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4102,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -71,9 +76,10 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4110);
 			player:messageSpecial(ITEM_OBTAINED,4110);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 	
 	if(trade:hasItemQty(4103,12) == true and trade:getGil() == 0 and trade:getItemCount() == 12) then
@@ -81,8 +87,9 @@ function mogCrystalTrader(player,npc,trade)
 			player:tradeComplete();
 			player:addItem(4111);
 			player:messageSpecial(ITEM_OBTAINED,4111);
-			
-	else
-		player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		else
+		
+			player:PrintToPlayer("Not enough inventory space. Please try again with at least 1 free slots");
+		end;
 	end;
 end;
